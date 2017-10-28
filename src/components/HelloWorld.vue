5f6ca2e6e78f4cacfdf776b8815cf60c9991d858
Andy <andystill@gmail.com>
2017-10-28 18:34:23 +0100

    Add popups to first page
    Update HelloWorld component to be firts page of system including annoyinf popups

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div>
+    So you want to make a complaint... 
+    <br/><br/>
+    Let's make this nice and easy...
+    <br/><br/>
+    First enter your name.
+    <br/><br/>
+    <input type="text" v-on:keydown="op" id="nameField">
+
+    <button type="button" class="btn btn-info btn-lg" id="btnNext">Next >>></button>
+
+
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+        
+              <!-- Modal content-->
+              <div class="modal-content">
+                  <div class="modal-header">
+                    <img src="assets/logo.png" style="hight:100;width:100">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">{{modalheader}}</h4>
+                  </div>
+                  <div class="modal-body">
+                  <div v-html="modaltext"></div>
+                  </div>
+                  <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  </div>
+              </div>
+        
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  data: function () {
+      return {
+        counter: 0,
+        annoyingintro:[
+          {header:"Welcome to Complaint-a-tron", 
+          text:`
+            <div>This form uses the revolutionary new Complain-a-tron technology</div>
+            <div>
+              Complain-a-tron has taken years of research to optimise the completion of complaint gathering.
+            </div>
+            <div>
+              We will help you every step of the way to ensure that completing this form is a pleasant experience.
+            </div>
+          `
+        },
+        {header:"Welcome to Complaint-a-tron", 
+          text:`
+            <div>As you carry on filling in the form. Keep your eyes peeled for "Mona" our on page helper.</div>
+            <div>"Mona" will always be ready with helpful hints</div>
+          `
+        },
+        {header:"Welcome to Complaint-a-tron", 
+          text:`
+            <div>Watch out for robots</div>
+            <div>For safety and security purposes we will be constantly scanning for robot activity you may be asked to validate that youb are not a robot</div>
+          `
+        },
+        {header:"Welcome to Complaint-a-tron", 
+          text:`
+            <div>!! WARNING!!!!</div>
+            <div>This is a serious process. Any partially completed forms will be regarded as a waste of government time which you may be charged for.</div>
+            <div>We may contact the authorities to re-capture wasted costs of this type</div>
+          `
+        }
+        ],
+        modaltext:"",
+        modalheader:""
+      }
+    },
+    created: function () {
+      // `this` points to the vm instance
+
+    },
+    methods: {
+      op:function(e){
+        if(this.counter<this.annoyingintro.length){
+          this.modaltext = this.annoyingintro[this.counter].text;
+          this.modalheader = this.annoyingintro[this.counter].header;
+          this.counter++;
+          $("#myModal").on('hidden.bs.modal', function () {
+            $("#nameField").focus();
+          });
+          $("#myModal").on('show.bs.modal', function () {
+            $("#nameField").blur();
+          });
+          $("#myModal").modal("show")
+          return false;
+        }
+      }
     }
-  }
 }
 </script>
 
