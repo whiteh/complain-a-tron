2424f12082c30622615f45d500aeb46623286791
Mark Greenwood <greenwma@googlemail.com>
2017-10-28 18:27:15 +0100

    Added eventbus

--- src/components/clippy.vue
+++ src/components/clippy.vue
@@ -4,19 +4,20 @@
 </template>
 
 <script>
+import { EventBus } from './events.js';
+EventBus.$on("test", function(){console.log("test fired");});
 export default {
     name: 'clippy',
     data () {
         return {};
       },
       mounted () {
-        console.log("mounted");
         this.selectHelper();
+        EventBus.$emit("test")
       },
       methods: {
         showHelper (helper) {
           var self = this;
-          console.log("in showHelper");
           helper = helper || this.selectedHelper;
           clippy.load(helper, function(agent){
               // do anything with the loaded agent
@@ -32,11 +33,12 @@ export default {
         selectHelper () {
           const agents = ['Merlin', 'Clippy', 'Rover', 'Links'],
                 index  = Math.floor(Math.random() * agents.length);
-          console.log("in selectHelper");
           this.showHelper(agents[index]);
         }
       }
     }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
