3bf8ef3ef0cf0f66fdc672a17867f26fe699ec57
Mark Greenwood <greenwma@googlemail.com>
2017-10-28 23:24:01 +0100

    Recaptcha feature

---
+++ src/components/recaptcha.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="captcha">
+    <vue-recaptcha class="recaptcha" ref="recaptcha" :sitekey="captcha_key" @verify="onVerify"></vue-recaptcha>
+    <div v-if="time>0" class="count">{{ time }}</div>
+    <clippy></clippy>
+  </div>
+</template>
+
+<script>
+  import VueRecaptcha from 'vue-recaptcha';
+  import clippy from '@/components/clippy';
+  import { EventBus } from './events.js';
+
+  /*setInterval(() => {
+        self.reset()
+      }, 10000);*/
+
+  export default {
+    data () {
+      return {
+        time: 0,
+        interval: null,
+        number_of_resets: 1,
+        captcha_key: process.env.CAPTCHA_KEY
+      }
+    },
+    methods: {
+      reset () {
+        this.$refs.recaptcha.reset()
+        this.number_of_resets+=1;
+      },
+      onVerify: function (response) {
+        var rand = Math.floor(Math.random() * 5);
+
+        if (rand === 1) {
+          this.reset()
+          EventBus.$emit("speak", "Sorry, you failed the test.  We don't believe you're human.  Please try again...")
+          return;
+        }
+        console.log('Verify: ' + response)
+        this.time = Math.ceil(15 * (1/this.number_of_resets));
+        clearInterval(this.interval);
+        this.interval = setTimeout(this.updateCount, 1000);
+      },
+      updateCount() {
+        this.time -= 1;
+        if (this.time === 0) {
+          EventBus.$emit("speak", "You've taken too long to submit the form.  Are you still human?")
+          this.reset();
+          clearInterval(this.interval);
+          this.interval = null;
+        } else {
+          this.interval = setTimeout(this.updateCount, 1000);
+        }
+      }
+    },
+    components: { VueRecaptcha, clippy }
+  };
+</script>
+<style scoped>
+div.captcha {
+  text-align: center;
+  margin: auto;
+  width: 100%;
+}
+div.captcha div.recaptcha {
+  padding-left:40%;
+}
+div.count {
+  font-size:24pt;
+  color: green;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Email from '@/components/Email'
+import Recaptcha from '@/components/recaptcha'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/Email',
       name: 'Email',
       component: Email
+    },
+    {
+      path: '/captcha',
+      name: 'captcha',
+      component: Recaptcha
     }
   ]
 })
