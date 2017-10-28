7d2f7079534646b5942d6b3a0376785e8de72c5d
Mark Greenwood <greenwma@googlemail.com>
2017-10-28 20:47:02 +0100

    Plugged in suggestion interval

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -106,11 +106,12 @@ export default {
       },
       suggestion () {
         const self = this,
-              interval = Math.floor(Math.random * 100000) + 3000;
+              interval = Math.floor(Math.random() * 100000) + 3000;
         this.interval = setInterval(function() { 
           clearInterval(self.interval);
           EventBus.$emit("suggest");
           self.suggestion() }, interval);
+        
       }
     },
     components: {
