252a3f8f138bd3a32688cb5ccd372eb59dde2b73
Andy <andystill@gmail.com>
2017-10-29 10:27:34 +0000

    Changes to finish page

--- src/components/finish.vue
+++ src/components/finish.vue
@@ -3,6 +3,10 @@
         Complaint upload attempted but may not have been successful.
         <br/><br/>
         Please call to check it has been received.
+        <br/><br/>
+        The number to call can be found by searching online.
+        <br/><hr width="50%"/><br/>
+        Thank you for using Complain-a-Tron, the future of complaint handling. We hope you enjoyed the experience.
     </div>
 </template>
 
